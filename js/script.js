40711e34c3e34a76299de0240870c01ae6d9656d
LuisMZampar <rm550531@fiap.com.br>
2023-05-03 22:56:03 -0300

    Button-Rgb com addEventListener Math getById.

--- js/script.js
+++ js/script.js
@@ -1,11 +1,39 @@
 
 
-var nome1 = "Petroni";
-// let nome2 = "Derick";
-// const nome3 = "Ruan";
+// var nome1 = "Petroni";
+// // let nome2 = "Derick";
+// // const nome3 = "Ruan";
 
-if(nome1 != ""){
-    let nome1 = "Geovana"
-}
+// if(nome1 != ""){
+//     let nome1 = "Geovana"
+// }
 
-console.log(nome1); 
+// console.log(nome1); 
+
+let botao = document.getElementById("meu-btn");
+console.log(botao.textContent);
+
+botao.addEventListener("click", function(){ 
+    // console.log(this.textContent);
+    //Função matemática MATH
+    //random = retorna um número aleatório entre 0 e 1. Ex: 0.765265;
+    //floor = Ela arredonda um número para baixo.
+    //ceil = Ela arredonda um número para cima.
+    //round = Ela arredonda um número aleatóriamente.
+
+    let r=0,g=0,b=0;
+    r = Math.round(Math.random()*255);
+    g = Math.round(Math.random()*255);
+    b = Math.round(Math.random()*255);
+
+    // this.setAttribute("style", "background-color:rgb("+r+", "+g+", "+b+");")
+    this.setAttribute("style", `background-color:rgb(${r},${g},${b});`);
+
+});
+
+//ESTUDAR:
+
+//querySelector
+//querySelectorA11
+//Array
+//Estruturas d erepetição 
