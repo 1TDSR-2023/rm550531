200acfda091264cc841cb26997c2cfb70a4c8512
LuisMZampar <rm550531@fiap.com.br>
2023-05-17 23:00:06 -0300

    Inserindo try catch e validação

--- js/login.js
+++ js/login.js
@@ -1,32 +1,127 @@
+const inputEmail = document.querySelector("#idEmail");
+
+inputEmail.addEventListener("keyup", () => {
+  const lblEmail = document.querySelector("label[for='idEmail']");
+
+  if (inputEmail.value.length < 5) {
+    inputEmail.setAttribute("style", "outline-color:#ff0000;");
+    lblEmail.setAttribute("style", "color:#ff0000;");
+  } else {
+    inputEmail.setAttribute("style", "outline-color:#00ff00;");
+    lblEmail.setAttribute("style", "color:#00ff00;");
+  }
+});
+
+const inputSenha = document.querySelector("#idPass");
+const eyePass = document.querySelector(".fa");
+
+eyePass.addEventListener("click", () => {
+  if (inputSenha.getAttribute("type") == "password") {
+    inputSenha.setAttribute("type", "text");
+    eyePass.setAttribute("class", "fa fa-eye-slash");
+  } else {
+    inputSenha.setAttribute("type", "password");
+    eyePass.setAttribute("class", "fa fa-eye");
+  }
+});
+
+//OBJETO
+//  const usuario1 = {
+//     nomeCompleto : "Luis Zampar",
+//     emailUsuario : "luis@gmail.com",
+//     senhaUsuario : "123456"
+//  }
+
+//  const usuario2 = {
+//     nomeCompleto : "Juan Palego",
+//     emailUsuario : "Juan@gmail.com",
+//     senhaUsuario : "123456"
+//  }
+
+//  let listaDeUsuarios = [];
+//  listaDeUsuarios.push(usuario1);
+//  listaDeUsuarios.push(usuario2);
+
+//LISTA DE USUARIOS
+
+let listaDeUsuarios = [
+  {
+    nomeCompleto: "Luis Zampar",
+    emailUsuario: "luis@gmail.com",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Juan Palego",
+    emailUsuario: "juan@gmail.com",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Donatao da massa",
+    emailUsuario: "donato@gmail.com",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Joelma farpas",
+    emailUsuario: "joelma@gmail.com",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Tonhao Neres",
+    emailUsuario: "neres@gmail.com",
+    senhaUsuario: "123456",
+  },
+  {
+    nomeCompleto: "Andre Zampar",
+    emailUsuario: "andre@gmail.com",
+    senhaUsuario: "123456",
+  },
+];
+
+const botaoSubmit = document.querySelector("#BtnSubmit");
+
+botaoSubmit.addEventListener("click", () => {
+  //CRIANDO UM OBJETO DE VALIDAÇÃO
+  let userValidado = {
+    emailUsuarioValidado: inputEmail.value,
+    senhaUsuarioValidado: inputSenha.value,
+  };
+
+  //CRIANDO UMA INTERAÇÃO COM A LISTA DE OBJETOS PARA
+  //REALIZAR A VALIDAÇÃO COM O usuarioValidade
+
+  try {
+    listaDeUsuarios.forEach((usuario) => {
+      //Realizando a validação
+      //Comparando
+      if (
+        userValidado.emailUsuarioValidado == usuario.emailUsuario &&
+        userValidado.senhaUsuarioValidado == usuario.senhaUsuario
+      ) {
+        userValidado = usuario;
+        throw "VALIDADO";
+      }
+    });
+
+    throw "SENHA OU NOME DE USUARIO INVÁLIDOS!";
+
+} catch (msg) {
+  
+    const msgStatus = document.querySelector("#msgStatus");
+
+    if (msg == "VALIDADO") {
+      msgStatus.setAttribute("style", "color:#00ff00");
+      msgStatus.innerHTML = `<span><strong>Usuário: ${userValidado.nomeCompleto} Login efetuado com sucesso</strong></span>`;
+
+      //Redirect
+      setTimeout(()=>{
+        window.location.href = "../home.html";
+      }, 3000);
 
 
-const inputEmail = document.querySelector("#idEmail")
-
-inputEmail.addEventListener("keyup", ()=>{
-
-    const lblEmail = document.querySelector("label[for='idEmail']")
-
-    if(inputEmail.value.length < 5){
-        inputEmail.setAttribute("style","outline-color:#ff0000;"); 
-        lblEmail.setAttribute("style","color:#ff0000;")
-    }else{
-        inputEmail.setAttribute("style","outline-color:#00ff00;"); 
-        lblEmail.setAttribute("style","color:#00ff00;")
+    } else {
+      msgStatus.setAttribute("style", "color:#ff0000");
+      msgStatus.innerHTML =
+        "<span><strong>Nome de usuario ou senha inválidos</strong></span>";
     }
-
- });
-
- const eyePass = document.querySelector(".fa");
-
- eyePass.addEventListener("click", ()=>{
-
-    const inputSenha = document.querySelector("#idPass");
-
-    if(inputSenha.getAttribute("type") == "password"){
-        inputSenha.setAttribute("type", "text");
-        eyePass.setAttribute("class","fa fa-eye-slash")
-    }else{
-        inputSenha.setAttribute("type", "password")
-        eyePass.setAttribute("class","fa fa-eye")
-    }
- });
+  }
+});
